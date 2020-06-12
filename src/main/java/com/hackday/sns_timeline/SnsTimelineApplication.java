af639ef30e29ab8c2a6e00ead27c77e2f2e823ba
KimYunsang-v <qwdbstkd@naver.com>
2020-06-12 18:59:21 +0900

    spring 제공 기능 확인

--- src/main/java/com/hackday/sns_timeline/SnsTimelineApplication.java
+++ src/main/java/com/hackday/sns_timeline/SnsTimelineApplication.java
@@ -11,6 +11,5 @@ public class SnsTimelineApplication {
 	public static void main(String[] args) {
 		System.setProperty("es.set.netty.runtime.available.processors", "false");
 		SpringApplication.run(SnsTimelineApplication.class, args);
-		System.setProperty("es.set.netty.runtime.available.processors", "false");
 	}
 }
